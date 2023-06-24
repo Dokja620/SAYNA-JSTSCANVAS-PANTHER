32e51f2cb99884f68cf6e80dd71f5321ede4f90c
Dokja620 <revanamike620@gmail.com>
2023-06-24 21:42:42 +0500

    Onscroll animation done...

--- scripts/page.js
+++ scripts/page.js
@@ -5,3 +5,19 @@ const navLinks = document.querySelector ('.nav-links')
 menuBurger.addEventListener('click', () => {
     navLinks.classList.toggle('mobile-menu')
 })
+
+//animation on scroll... Using next-gen css feature
+//Intersection Observer... It tell if an element is visible in the DOM
+//I'll apply style to it
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('appear')
+        } else{
+            entry.target.classList.remove('appear')
+        }
+    })
+})
+const toscrollElements = document.querySelectorAll('.scroll')
+toscrollElements.forEach((el) => observer.observe(el))
