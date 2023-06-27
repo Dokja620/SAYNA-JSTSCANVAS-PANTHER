2cee32e6f2c85b02d1fd39a4e5fccea8e5c148b2
Dokja620 <revanamike620@gmail.com>
2023-06-27 11:50:38 +0500

    Aligning JavaScript content

--- scripts/page.js
+++ scripts/page.js
@@ -1,10 +1,18 @@
-//Nav bar
-const menuBurger = document.querySelector('.menu-burger')
-const navLinks = document.querySelector ('.nav-links')
-
-menuBurger.addEventListener('click', () => {
-    navLinks.classList.toggle('mobile-menu')
+//animation on scroll... Using next-gen css feature
+//Intersection Observer... It tell if an element is visible in the DOM
+//I'll apply style to it
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('appear')
+        } else{
+            entry.target.classList.remove('appear')
+        }
+    })
 })
+const toscrollElements = document.querySelectorAll('.scroll')
+toscrollElements.forEach((el) => observer.observe(el))
 
 //custom cursor
 const cursor = document.querySelector('.cursor')
@@ -15,12 +23,20 @@ document.addEventListener("mousemove", e =>{
 
 document.addEventListener('click', () =>{
     cursor.classList.add("expand")
-    
     setTimeout(() => {
         cursor.classList.remove("expand")
     }, 500);
 })
-//Validation popup
+
+//Nav bar
+const menuBurger = document.querySelector('.menu-burger')
+const navLinks = document.querySelector ('.nav-links')
+
+menuBurger.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu')
+})
+
+//Form validation and form popup
 function showPopup() {
     let nameInput = document.getElementById("name")
     let emailInput = document.getElementById("email")
@@ -42,27 +58,15 @@ function showPopup() {
     //clear form inputs after submission
     nameInput.value = ""
     emailInput.value = ""
-    messageInput.value = ""
-    
-    return false
+    messageInput.value = ""  
+  return false
 }
+
 function closePopup(){
     let popup = document.getElementById("popup")
     popup.style.display = "none"
 }
 
-//animation on scroll... Using next-gen css feature
-//Intersection Observer... It tell if an element is visible in the DOM
-//I'll apply style to it
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach((entry) =>{
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('appear')
-        } else{
-            entry.target.classList.remove('appear')
-        }
-    })
-})
-const toscrollElements = document.querySelectorAll('.scroll')
-toscrollElements.forEach((el) => observer.observe(el))
+//Home page slider
+
+//Guessing game Enigma page
